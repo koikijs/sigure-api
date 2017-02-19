135777f77762c90dfa6c9950eb930c886475fb07
nabuchi <nabnab.main+github@gmail.com>
2017-02-19 15:51:26 +0900

    add tag test

--- src/main/java/koikijs/model/tag/Tag.java
+++ src/main/java/koikijs/model/tag/Tag.java
@@ -1,5 +1,7 @@
 package koikijs.model.tag;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
@@ -7,6 +9,7 @@ import javax.persistence.Id;
  * Created by nabuchi on 2017/02/19.
  */
 @Entity
+@Data
 public class Tag {
     @Id
     private String id;
--- src/test/java/koikijs/model/tag/TagRepositoryTest.java
+++ src/test/java/koikijs/model/tag/TagRepositoryTest.java
@@ -3,6 +3,7 @@ package koikijs.model.tag;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.*;
@@ -11,12 +12,18 @@ import static org.junit.Assert.*;
  * Created by nabuchi on 2017/02/19.
  */
 @RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class TagRepositoryTest {
     @Autowired
     TagRepository tagRepository;
 
     @Test
     public void test() {
-        System.out.println(33);
+        Tag tag = new Tag();
+        tag.setId("id");
+        tag.setName("name");
+        tagRepository.save(tag);
+        Tag savedTag = tagRepository.findOne("id");
+        System.out.println(savedTag);
     }
 }
