891d90cd943d6e489e8ce0b1ba6c8abb10e787b6
nabuchi <nabnab.main+github@gmail.com>
2017-02-19 14:34:13 +0900

    Initial Commit

---
+++ src/main/java/koikijs/SigureApiApplication.java
@@ -0,0 +1,12 @@
+package koikijs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigureApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigureApiApplication.class, args);
+	}
+}
---
+++ src/test/java/koikijs/SigureApiApplicationTests.java
@@ -0,0 +1,16 @@
+package koikijs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SigureApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
